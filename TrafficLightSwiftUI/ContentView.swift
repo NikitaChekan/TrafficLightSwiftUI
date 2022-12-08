f15e323f94ecd4b0e5f638ffcd040c53d870a6f8
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-12-08 14:21:10 +0300

    moved button in content view

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,19 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var buttonTitle = "START"
+    private let changeColor = ChangeColor.red
+    
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.3
+    
     var body: some View {
-        VStack {
-            RedCircleView()
-                .padding()
-            YellowCircleView()
-                .padding()
-            GreenCircleView()
-                .padding()
-            Spacer()
-            CustomButtonView()
-                .padding()
+        ZStack {
+            Color(.black)
+                .ignoresSafeArea()
+            VStack {
+                RedCircleView()
+                YellowCircleView()
+                    .padding()
+                GreenCircleView()
+
+                Spacer()
+                
+                Button(action: changeColorButtonDidTapped) {
+                    Text(buttonTitle)
+                        .font(.title)
+                        .foregroundColor(.blue)
+                        .fontWeight(.medium)
+                }
+                .tint(.blue)
+                .buttonStyle(.bordered)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+            }
+            .padding()
         }
     }
+    
+    private func changeColorButtonDidTapped() {
+//        switch changeColor {
+//        case .red:
+//            greenCircleView.alpha = lightOff
+//            redCircleView.alpha = lightOn
+//            changeColor = .yellow
+//        case .yellow:
+//            redCircleView.alpha = lightOff
+//            yellowCircleView.alpha = lightOn
+//            changeColor = .green
+//        case .green:
+//            yellowCircleView.alpha = lightOff
+//            greenCircleView.alpha = lightOn
+//            changeColor = .red
+//        }
+    }
+}
+
+private enum ChangeColor {
+    case red, yellow, green
 }
 
 struct ContentView_Previews: PreviewProvider {
